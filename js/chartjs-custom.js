91cb64167b9ec591d4d7ed97ebcc53d440d14083
Alex Jose <alexjosesilvati@gmail.com>
2020-01-19 02:09:23 -0300

    dev

--- js/chartjs-custom.js
+++ js/chartjs-custom.js
@@ -112,14 +112,16 @@ $(document).ready(function() {
                 strokeColor : "rgba(220,220,220,1)",
                 pointColor : "rgba(220,220,220,1)",
                 pointStrokeColor : "#fff",
-                data : [65,59,90,81,56,55,40]
+                data : [65,59,90,81,56,55,40],
+                label: "Pe Esquerdo",
             },
             {
                 fillColor : "rgba(151,187,205,0.5)",
                 strokeColor : "rgba(151,187,205,1)",
                 pointColor : "rgba(151,187,205,1)",
                 pointStrokeColor : "#fff",
-                data : [28,48,40,19,96,27,100]
+                data : [28,48,40,19,96,27,100],
+                label: "Pe Direito",
             }
         ]
 
